4a7394a05ee2b89b0f750a82c33b127a8e1976b6
Disha Mehta <43294150+dishamehta234@users.noreply.github.com>
2021-02-04 18:13:48 +0530

    Changes done till route

--- sales.js
+++ sales.js
@@ -1,92 +1,104 @@
 /*(function () {
   console.log("Sales Distribution System", owl.__info__.version);
 })();*/
-
-const { Component, mount } = owl;
+const { Component, Store, mount, qweb} = owl;
 const { xml } = owl.tags;
 const { whenReady } = owl.utils;
+const { useRef, useDispatch, useState, useStore } = owl.hooks;
+
+const SALES_TEMPLATE = xml/* xml */ `
+    	<div>
+			<header>
+		  	<nav class="navbar navbar-dark bg-primary fixed-top">
+		  			<p class="text-left text-uppercase font-italic text-white">
+		  				<h2>Sales Distribution</h2>
+		  			</p>
+		  		<form class="form-inline">
+		    		<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+		    		<button class="btn btn-outline-primary bg-light my-2 my-sm-0" type="submit">Search</button>
+		  		</form>
+			</nav>
+		</header>
+		<footer>
+
+		</footer>
+		</div>`;
 
 // Owl Components
-class App extends Component {
-  static template = xml`<body>
-  	<div class="mobile_overlay"></div>
-  	<div class="mobile_menu">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="bar_open">
-                        <a href="#"><i class="fas fa-bars"></i></a>
-                    </div>
-                    <div class="mobile_wrapper">
-                        <div class="bar_close">
-                            <a href="#"><i class="fas fa-times"></i></a>
-                        </div>
-                        <div class="freeshipping">
-                            <p>You can get Free Delivery By Shopping More Than Free</p>
-                        </div>
+export class sales extends Component {
+  static template = SALES_TEMPLATE;
+}
+const SIGNUP_TEMPLATE = xml/* xml */ `
+    	<div>
+			<!-- signup form -->
+			<form class="form-signin">
+		      <img class="mb-4" src="../logo.png" alt="logo" width="72" height="72" />
+		      <h1 class="h3 mb-3 font-weight-normal">Sign Up</h1>
+		      <label for="Email" class="sr-only">Email ID</label>
+		      <input type="email" id="Email" class="form-control" placeholder="EmailId" required="" autofocus="" />
 
-                        <div class="header_social_icon text-center">
-                            <ul>
-                                <li class="social-icon">
-                                    <a target="_blank" href="https://www.facebook.com/">
-                                    	<em class="fab fa-facebook"></em>
-                                    </a>
-                                </li>
-                                <li class="social-icon">
-                                    <a target="_blank" href="https://whatsapp.com/"><em class="fab fa-whatsapp"></em></a>
-                                </li>
-                                <li class="social-icon">
-                                    <a target="_blank" href="https://www.instagram.com/"><em class="fab fa-instagram"></em></a>
-                                </li>
-                                <li class="social-icon">
-                                        <a target="_blank" href="https://twitter.com/"><em class="fab fa-twitter"></em></a>
-                                </li>
-                                <li class="social-icon">
-                                        <a target="_blank" href="https://www.skype.com/"><em class="fab fa-skype"></em></a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="header_call-support">
-                            <p><a href="#">+91 56498778233</a> Customer Support</p>
-                        </div>
-                        <div id="menu" class="text-left ">
-                            <ul class="header_main_menu">
-                                <li class="header_submenu_item active">
-                                    <a href="#">Home</a>
-                                </li>
+		      <label for="mno" class="sr-only">Mobile Number</label>
+		      <input type="number" id="mno" class="form-control" placeholder="Mobile Number" required="" autofocus="" />
 
-                                <li class="header_submenu_item">
-                                    <a href="#"> Contact Us</a>
-                                </li>
+		      <label for="pass" class="sr-only">Password</label>
+		      <input type="password" id="pass" class="form-control" placeholder="Password" required="" />
 
-                                <li class="header_submenu_item">
-                                    <a href="#"> Shop</a>
-                                </li>
+		      <label for="CnfPassword" class="sr-only">Conform Password</label>
+		      <input type="password" id="CnfPassword" class="form-control" placeholder="Conform Password" required="" />
 
-                                <li class="header_submenu_item"><span class="menu-expand"><i class="fa fa-angle-down"></i></span>
-                                    <a href="#"> More</a>
-                                    <ul class="sub-menu" style="display: none;">
-                                        <li><a href="#">About Us</a></li>
-                                        <li><a href="#">FAQ</a></li>
+		      <div class="checkbox mb-3">
+		        <label>
+		          <input type="checkbox" value="remember-me" /> Remember me
+		        </label>
+		      </div>
+		      <button class="btn btn-lg btn-primary btn-block" type="submit" t-on-click="sign_up_function">Sign Up</button>
+		      <span>
+				<t t-as="error"/>
+			  </span>
+		    </form>
+		</div>`;
 
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="offcanvas_footer">
-                            <span><a href="#"><i class="fa fa-envelope"></i> Tiny Erp Odoo India</a></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
- </body>`;
+class signup extends Component{
+	static template = SIGNUP_TEMPLATE;
+
+	sign_up_function(){
+		var email ,m_no,pswd,crfpswd;
+		email = document.getElementById("Email").value;
+		m_no = document.getElementById("mno").value;
+		pswd = document.getElementById("pass").value;
+		crfpswd = document.getElementById("CnfPassword").value;
+
+		if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(formdata.email.value) && /^\d{10}$/)
+        {
+        		document.getElementById("error").value="Valid details";
+        }	
+        else{
+        		document.getElementById("error").value="InValid details";
+        }		
+	}
 }
 
+const ROUTES = [
+  { name: "signup", path: "/signup", component: signup }
+];
+async function protectRoute({ env, to }) {
+  if (!env.session.authUser) {
+    env.session.setNextRoute(to.name);
+    return { to: "signup" };
+  }
+  return true;
+}
+async function makeEnvironment() {
+    const env = { qweb };
+    env.router = new owl.router.Router(env, ROUTES);
+    await env.router.start();
+    return env;
+}
+
+sales.env = makeEnvironment();
 // Setup code
 function setup() {	
-		const app = new App();
+		const app = new sales();
 		app.mount(document.body);
 }
 
